c1e4e5561e2129a8bdcc5397864d58129a59b225
Maxime Rainville <maxime@silverstripe.com>
2021-04-07 15:39:45 +1200

    API Refactor LoginSessionContainer to be a functional component.

--- client/src/components/LoginSession/LoginSessionContainer.js
+++ client/src/components/LoginSession/LoginSessionContainer.js
@@ -1,80 +1,65 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import backend from 'lib/Backend';
 import Config from 'lib/Config'; // eslint-disable-line
 import LoginSession from './LoginSession';
 import { success, error } from 'state/toasts/ToastsActions';
+import i18n from 'i18n';
 
-// Handle communication with  server on logout and toast notifications via redux
-// Using a class component rather than function component so that LoginSessionContainer-test.js
-// can use wrapper.instance() to test logout().
-// .instance() does not work on stateless functional components
-// https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/instance.html
-class LoginSessionContainer extends Component {
-    //
-    constructor(props) {
-        super(props);
-        this.createEndpoint = this.createEndpoint.bind(this);
-        this.logout = this.logout.bind(this);
-    }
+function createEndpoint(logOutEndpoint) {
+    return backend.createEndpointFetcher({
+        url: `${logOutEndpoint}/:id`,
+        method: 'delete',
+        payloadSchema: {
+            id: { urlReplacement: ':id', remove: true },
+            SecurityID: { querystring: true }
+        }
+    });
+}
 
-    componentWillMount() {
-        this.setState({
-            complete: false,
-            failed: false,
-            submitting: false
-        });
-    }
+/**
+ * Handle communication with  server on logout and toast notifications via redux.
+ * @param props
+ */
+function LoginSessionContainer(props) {
+    const [complete, setComplete] = useState(false);
+    const [failed, setFailed] = useState(false);
+    const [submitting, setSubmitting] = useState(false);
 
-    createEndpoint() {
-        return backend.createEndpointFetcher({
-            url: `${this.props.LogOutEndpoint}/:id`,
-            method: 'delete',
-            payloadSchema: {
-                id: { urlReplacement: ':id', remove: true },
-                SecurityID: { querystring: true }
-            }
-        });
-    }
-
-    logout() {
-        this.setState({
-            ...this.state,
-            submitting: true
-        });
-        const endpoint = this.createEndpoint();
-        endpoint({
-            id: this.props.ID,
+    function logout() {
+        setSubmitting(true);
+        const endpoint = createEndpoint(props.LogOutEndpoint);
+        return endpoint({
+            id: props.ID,
             SecurityID: Config.get('SecurityID')
         })
         .then(response => {
-            const failed = !response.success;
-            this.setState({
-                complete: true,
-                failed,
-                submitting: false
-            });
-            if (failed) {
-                this.props.displayToastFailure(response.message);
+            const failure = !response.success;
+            setFailed(failure);
+
+            if (failure) {
+                props.displayToastFailure(response.message);
             } else {
-                this.props.displayToastSuccess(response.message);
+                props.displayToastSuccess(response.message);
             }
         })
         .catch(() => {
-            this.setState({
-                complete: true,
-                failed: true,
-                submitting: false
-            });
+            setFailed(true);
+            props.displayToastFailure(i18n._t(
+                'SessionManager.COULD_NOT_LOGOUT',
+                'Could not log out of session. Try again later.'
+            ));
+        })
+        .finally(() => {
+            setComplete(true);
+            setSubmitting(false);
         });
     }
 
-    render() {
-        const { ID, LogoutEndPoint, ...loginSessionProps } = this.props;
-        const newProps = { logout: this.logout, ...this.state, ...loginSessionProps };
-        return <LoginSession {...newProps} />;
-    }
+    const { ID, ...loginSessionProps } = props;
+    const newProps = { logout, complete, failed, submitting, ...loginSessionProps };
+    return <LoginSession {...newProps} />;
 }
 
 LoginSessionContainer.propTypes = {
