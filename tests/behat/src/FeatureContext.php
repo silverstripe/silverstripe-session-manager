6d1d40bb2ee8cd0646c002e2a77f7557344e0717
Steve Boyd <emteknetnz@gmail.com>
2021-04-12 15:04:31 +1200

    MNT Add behat to module

---
+++ tests/behat/src/FeatureContext.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SilverStripe\SessionManager\Tests\Behat\Context;
+
+use SilverStripe\BehatExtension\Context\SilverStripeContext;
+
+class FeatureContext extends SilverStripeContext
+{
+}
