85d818c89fcf10062a2b7eb258433aa8c02a322c
Steve Boyd <emteknetnz@gmail.com>
2021-04-20 17:43:07 +1200

    FIX Revoke a single session

--- src/Middleware/LoginSessionMiddleware.php
+++ src/Middleware/LoginSessionMiddleware.php
@@ -5,10 +5,12 @@ namespace SilverStripe\SessionManager\Control;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Control\Middleware\HTTPMiddleware;
+use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\ORM\Connect\DatabaseException;
 use SilverStripe\ORM\FieldType\DBDatetime;
 use SilverStripe\Security\IdentityStore;
+use SilverStripe\Security\RememberLoginHash;
 use SilverStripe\Security\Security;
 use SilverStripe\SessionManager\Model\LoginSession;
 use SilverStripe\SessionManager\Security\LogInAuthenticationHandler;
@@ -35,6 +37,7 @@ class LoginSessionMiddleware implements HTTPMiddleware
             // If the session has already been revoked, or we've got a mismatched
             // member / session, log the user out (this also revokes the session)
             if (!$loginSession || (int)$loginSession->MemberID !== (int)$member->ID) {
+                RememberLoginHash::setLogoutAcrossDevices(false);
                 $identityStore = Injector::inst()->get(IdentityStore::class);
                 $identityStore->logOut($request);
                 return $delegate($request);
