986e80d87323e7c23217274678516c465edec818
Thomas Portelange <thomas@lekoala.be>
2023-06-28 16:05:40 +0200

    use maxAge method

--- src/Services/GarbageCollectionService.php
+++ src/Services/GarbageCollectionService.php
@@ -3,6 +3,7 @@
 namespace SilverStripe\SessionManager\Services;
 
 use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\ORM\FieldType\DBDatetime;
 use SilverStripe\Security\RememberLoginHash;
 use SilverStripe\SessionManager\Models\LoginSession;
 
@@ -25,12 +26,11 @@ class GarbageCollectionService
      */
     private function collectExpiredSessions(): void
     {
-        $lifetime = LoginSession::config()->get('default_session_lifetime');
-        $sessions = LoginSession::get()->filter([
-            'LastAccessed:LessThan' => date('Y-m-d H:i:s', time() - $lifetime),
+        $maxAge = LoginSession::getMaxAge();
+        LoginSession::get()->filter([
+            'LastAccessed:LessThan' => $maxAge,
             'Persistent' => 0
-        ]);
-        $sessions->removeAll();
+        ])->removeAll();
     }
 
     /**
@@ -38,9 +38,18 @@ class GarbageCollectionService
      */
     private function collectImplicitlyExpiredSessions(): void
     {
+        $now = DBDatetime::now()->getTimestamp();
         LoginSession::get()->filter([
             'Persistent' => 1,
-            'LoginHash.ExpiryDate:LessThan' => date('Y-m-d H:i:s')
+            'LoginHash.ExpiryDate:LessThan' => date('Y-m-d H:i:s', $now),
+        ])->removeAll();
+
+        $maxAge = LoginSession::getMaxAge();
+        // If a persistent session has no login hash, use LastAccessed
+        LoginSession::get()->filter([
+            'LastAccessed:LessThan' => $maxAge,
+            'Persistent' => 1,
+            'LoginHash.ExpiryDate' => null,
         ])->removeAll();
     }
 
@@ -49,8 +58,9 @@ class GarbageCollectionService
      */
     private function collectExpiredLoginHashes(): void
     {
+        $now = DBDatetime::now()->getTimestamp();
         RememberLoginHash::get()->filter([
-            'ExpiryDate:LessThan' => date('Y-m-d H:i:s')
+            'ExpiryDate:LessThan' => date('Y-m-d H:i:s', $now),
         ])->removeAll();
     }
 }
