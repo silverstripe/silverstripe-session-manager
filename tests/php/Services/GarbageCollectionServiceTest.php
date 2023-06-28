cef06282c4edbaa71064dc3b83b0347e0a61ca30
Thomas Portelange <thomas@lekoala.be>
2023-06-28 15:28:47 +0200

    actually show that mock time does something

--- tests/php/Services/GarbageCollectionServiceTest.php
+++ tests/php/Services/GarbageCollectionServiceTest.php
@@ -21,7 +21,7 @@ class GarbageCollectionServiceTest extends SapphireTest
 
     public function testGarbageCollection()
     {
-        DBDatetime::set_mock_now('2003-08-15 12:00:00');
+        DBDatetime::set_mock_now('2003-05-16 12:00:00');
 
         $id1 = $this->objFromFixture(LoginSession::class, 'x1')->ID;
         $id2 = $this->objFromFixture(LoginSession::class, 'x2')->ID;
@@ -34,13 +34,29 @@ class GarbageCollectionServiceTest extends SapphireTest
             LoginSession::get()->byID($id1),
             "Expired login session is deleted"
         );
+        // ExpiryDate for the hash is set to '2003-05-15 10:00:00' => it should be deleted
         $this->assertNull(
             LoginSession::get()->byID($id2),
             "Expired persistent login hash session is deleted"
         );
+        // LastAccessed is set to '2004-02-15 10:00:00' => it should not be deleted
         $this->assertNotNull(
             LoginSession::get()->byID($id3),
             "Valid login session is not deleted"
+        );
+        $this->assertEquals(
+            0,
+            LoginSession::get()->byID($id3)->LoginHash()->ID,
+            "Hash is deleted but session is still valid"
+        );
+
+        DBDatetime::set_mock_now('2005-08-15 12:00:00');
+
+        $garbageCollectionService->collect();
+
+        $this->assertNull(
+            LoginSession::get()->byID($id3),
+            "Login session is now deleted"
         );
     }
 }
