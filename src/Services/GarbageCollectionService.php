348fe08e172d9bada2517032cf1c44c3a6f48bdd
Thomas Portelange <thomas@lekoala.be>
2023-06-28 15:33:42 +0200

    allow mock time in garbage service

--- src/Services/GarbageCollectionService.php
+++ src/Services/GarbageCollectionService.php
@@ -3,6 +3,7 @@
 namespace SilverStripe\SessionManager\Services;
 
 use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\ORM\FieldType\DBDatetime;
 use SilverStripe\Security\RememberLoginHash;
 use SilverStripe\SessionManager\Models\LoginSession;
 
@@ -26,11 +27,11 @@ class GarbageCollectionService
     private function collectExpiredSessions(): void
     {
         $lifetime = LoginSession::config()->get('default_session_lifetime');
-        $sessions = LoginSession::get()->filter([
-            'LastAccessed:LessThan' => date('Y-m-d H:i:s', time() - $lifetime),
+        $now = DBDatetime::now()->getTimestamp() - $lifetime;
+        LoginSession::get()->filter([
+            'LastAccessed:LessThan' => date('Y-m-d H:i:s', $now),
             'Persistent' => 0
-        ]);
-        $sessions->removeAll();
+        ])->removeAll();
     }
 
     /**
@@ -38,9 +39,19 @@ class GarbageCollectionService
      */
     private function collectImplicitlyExpiredSessions(): void
     {
+        $now = DBDatetime::now()->getTimestamp();
         LoginSession::get()->filter([
             'Persistent' => 1,
-            'LoginHash.ExpiryDate:LessThan' => date('Y-m-d H:i:s')
+            'LoginHash.ExpiryDate:LessThan' => date('Y-m-d H:i:s', $now),
+        ])->removeAll();
+
+        $lifetime = LoginSession::config()->get('default_session_lifetime');
+        $now = DBDatetime::now()->getTimestamp() - $lifetime;
+        // If a persistent session has no login hash, use LastAccessed
+        LoginSession::get()->filter([
+            'LastAccessed:LessThan' => date('Y-m-d H:i:s', $now),
+            'Persistent' => 1,
+            'LoginHash.ExpiryDate' => null,
         ])->removeAll();
     }
 
@@ -49,8 +60,9 @@ class GarbageCollectionService
      */
     private function collectExpiredLoginHashes(): void
     {
+        $now = DBDatetime::now()->getTimestamp();
         RememberLoginHash::get()->filter([
-            'ExpiryDate:LessThan' => date('Y-m-d H:i:s')
+            'ExpiryDate:LessThan' => date('Y-m-d H:i:s', $now),
         ])->removeAll();
     }
 }
