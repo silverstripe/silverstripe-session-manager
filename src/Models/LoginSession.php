6679c50f1cc4a485c5086565da03c8c0648e0e8d
Thomas Portelange <thomas@lekoala.be>
2023-06-30 08:36:33 +0200

    Update src/Models/LoginSession.php
    Co-authored-by: Michal Kleiner <mk@011.nz>

--- src/Models/LoginSession.php
+++ src/Models/LoginSession.php
@@ -269,7 +269,7 @@ class LoginSession extends DataObject
 
         $loginHandler = Injector::inst()->get(LogInAuthenticationHandler::class);
         $loginSessionID = $request->getSession()->get($loginHandler->getSessionVariable());
-        $loginSession = DataObject::get_by_id(LoginSession::class, $loginSessionID);
+        $loginSession = LoginSession::get_by_id($loginSessionID);
         return $loginSession;
     }
 
