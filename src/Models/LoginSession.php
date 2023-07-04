ad7265d6525e15d8cd592dbac6bd7e77fb5e2295
Thomas Portelange <thomas@lekoala.be>
2023-07-04 23:11:15 +0200

    ENH get cached Member in LoginSession (#152)
    calls to isCurrent do not benefit from cache when byID is used

--- src/Models/LoginSession.php
+++ src/Models/LoginSession.php
@@ -260,7 +260,7 @@ class LoginSession extends DataObject
 
         $loginHandler = Injector::inst()->get(LogInAuthenticationHandler::class);
         $loginSessionID = $request->getSession()->get($loginHandler->getSessionVariable());
-        $loginSession = LoginSession::get()->byID($loginSessionID);
+        $loginSession = LoginSession::get_by_id($loginSessionID);
         return $loginSession;
     }
 
