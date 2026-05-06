57ff12bad7c1ddacf58277a524e01908695899c3
Sunny Side Up <github@sunnysideup.co.nz>
2026-05-06 12:01:37 +1200

    Fix: type casting for LastAccessed comparison

--- src/Middleware/LoginSessionMiddleware.php
+++ src/Middleware/LoginSessionMiddleware.php
@@ -48,7 +48,7 @@ class LoginSessionMiddleware implements HTTPMiddleware
             // Update LastAccessed date and IP address if > that threshold
             $date = DBDatetime::now()->Rfc2822();
             $threshold = LoginSession::config()->get('last_accessed_threshold');
-            if (strtotime($date) > strtotime($loginSession->LastAccessed) + $threshold) {
+            if (strtotime($date) > strtotime((string) $loginSession->LastAccessed) + $threshold) {
                 $loginSession->updateLastAccessed($request);
             }
         } catch (DatabaseException $e) {
